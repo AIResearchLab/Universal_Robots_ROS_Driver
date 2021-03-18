10cd70242a68d5317fd910b41fc71a9f96302b71
Dylan Morley <u3201390@uni.canberra.edu.eu>
2021-03-18 05:48:03 +0000

    Simulated UR10 config adjustment

---
+++ ur_robot_driver/scripts/URI-S.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import rospy
+import time
+from control_msgs.msg import JointTrajectoryControllerState
+
+def callback(data):
+     rospy.loginfo(data)
+     
+def listener():
+
+    rospy.init_node('uri-s', anonymous=True)
+    rospy.Subscriber("arm_controller/state", JointTrajectoryControllerState, callback)
+    rospy.spin()
+if __name__ == '__main__':
+    listener()

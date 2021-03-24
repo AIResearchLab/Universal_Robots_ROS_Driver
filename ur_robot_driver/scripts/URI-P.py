a86a0b7168509b2d57c5fb24d967a7daf0c8e1b1
Dylan Morley <u3201390@uni.canberra.edu.eu>
2021-03-24 07:43:22 +0000

    Iteration Between Waypoints Added

--- ur_robot_driver/scripts/URI-P.py
+++ ur_robot_driver/scripts/URI-P.py
@@ -4,26 +4,35 @@ import time
 import numpy
 from std_msgs.msg import Float64MultiArray
 
+def home():
+    executeck = raw_input("Execute Current Waypoints? (y/n): ")
+    if executeck == "y":
+        talker()
+    else:
+        waypointck = raw_input("Add Waypoints? (y/n): ")
+        if waypointck == "y":
+            print("Not Supported on Sim")
+            home()
+        else:
+            exit()
+
 def talker():
     pub = rospy.Publisher('/joint_group_position_controller/command', Float64MultiArray, queue_size=10)
     rospy.init_node('urcom', anonymous=True)
     rate = rospy.Rate(50)
-    Waypoints1 = [0.00, 0.00, 0.00, 0.00, 0.00, 0.00]
-    Waypoints2 = [0.00, -1.00, 1.00, 0.00, 1.57, 0.00]
+    Waypoints = [[0.00, 0.00, 0.00, 0.00, 0.00, 0.00],[0.00, -1.00, 1.00, 0.00, 1.57, 0.00],[0.00, -1.00, 1.00, 0.00, 3.14, 0.00],[0.00, 0.00, 0.00, 0.00, 0.00, 0.00]]
+    wpn = len(Waypoints)
+    wpc = 0
     while not rospy.is_shutdown():
-        Pos1 = Float64MultiArray()
-        Pos1.data = Waypoints1
-        Pos2 = Float64MultiArray()
-        Pos2.data = Waypoints2
-        #check if user wants to add another waypoint to the sequence
-        executeck = raw_input("Execute Current Waypoints? (y/n): ")
-        if executeck == "y":
-            pub.publish(Pos1)
-            time.sleep(4)
-            pub.publish(Pos2)
-            time.sleep(4)
-        else:
-            rate.sleep()
-        
+        Pos = Float64MultiArray()
+        while wpc < wpn:
+            print("Executing Waypoint: ", wpc)
+            Pos.data = Waypoints[wpc]
+            pub.publish(Pos)
+            time.sleep(1.5)
+            print("Reached Waypoint: ", wpc)
+            wpc = wpc+1
+            if(wpc == (wpn)):
+                home()
 if __name__ == '__main__':
-    talker()
+    home()

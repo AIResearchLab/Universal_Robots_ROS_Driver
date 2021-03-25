1a323ec1746060785f8ee56c708de2a9b3c7b79a
Dylan Morley <u3201390@uni.canberra.edu.eu>
2021-03-25 05:05:48 +0000

    Automated Controller Handover & Refined Startup Publisher

--- ur_robot_driver/scripts/URI-P.py
+++ ur_robot_driver/scripts/URI-P.py
@@ -2,9 +2,15 @@
 import rospy
 import time
 import numpy
+from controller_manager_msgs.srv import SwitchController
 from std_msgs.msg import Float64MultiArray
 
+
 def home():
+    rospy.wait_for_service('/controller_manager/switch_controller')
+    controller = rospy.ServiceProxy('/controller_manager/switch_controller', SwitchController)
+    consu = controller(start_controllers=['joint_group_position_controller'],stop_controllers=['arm_controller'] ,strictness=1, start_asap= False, timeout= 0.0)
+    print(consu)
     executeck = raw_input("Execute Current Waypoints? (y/n): ")
     if executeck == "y":
         talker()
@@ -22,7 +28,7 @@ def talker():
     rate = rospy.Rate(50)
     homepos = Float64MultiArray(data=[0.00, 0.00, 0.00, 0.00, 0.00, 0.00])
     pub.publish(homepos)
-    addway = [[0.00, -1.00, 1.00, 1.00, 1.57, 1.00],[0.00, -1.00, 1.00, 0.00, 3.14, 0.00],[0.00, 0.00, 0.00, 0.00, 0.00, 0.00]]
+    addway = [[0.00, -1.00, 1.00, 1.00, 1.57, 1.00],[4.28, -1.00, 1.00, -3.14, -3.14, 0.00],[0.00, -1.00, 1.00, 4.28, -1.57, 0.00],[0.00, 0.00, 0.00, 0.00, 0.00, 0.00]]
     Waypoints = []
     Waypoints = Waypoints+addway
     print("Available Waypoints: ", Waypoints)
@@ -34,7 +40,7 @@ def talker():
             print("Executing Waypoint: ", wpc)
             Pos.data = Waypoints[wpc]
             pub.publish(Pos)
-            time.sleep(1.5)
+            time.sleep(2)
             print("Reached Waypoint: ", wpc)
             wpc = wpc+1
             if(wpc == (wpn)):
